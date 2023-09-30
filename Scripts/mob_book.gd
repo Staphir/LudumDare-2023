14dbc181285802acf8046925704236348ad7279e
cugnett <cugnet.theophile@gmail.com>
2023-10-01 01:06:48 +0200

    Adding different books type

--- Scripts/mob_book.gd
+++ Scripts/mob_book.gd
@@ -1,9 +1,14 @@
-extends RigidBody2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#hide() #initilly hidden
+	var textures = [
+		load("res://Objects/Textures/book1.png"),
+		load("res://Objects/Textures/book2.png"),
+		load("res://Objects/Textures/book3.png")
+		]
+	$Sprite2D.texture = textures[randi_range(0,textures.size()-1)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/mob_book_grid.gd
+++ Scripts/mob_book_grid.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 @export var mob_book_scene: PackedScene
-@export var shelf_y_pos = 180
+@export var shelf_y_pos = 120
 @export var nb_mob_books = 6 #Max is 8
 var nb_slots = 8
-var init_pos_x = 205
+var init_pos_x = 182
 var book_size = 52
 var spacing = 4
 var middle_spacing = 35
@@ -31,5 +31,9 @@ func spawn_mob_book():
 		var book_x_pos = init_pos_x + book_size * slot + spacing
 		if slot >= 4: #second part of the library
 			book_x_pos += middle_spacing
+			
+		
+		print(mob_book.get_node("Sprite2D").texture.get_size().y)
 		mob_book.position = Vector2(book_x_pos,shelf_y_pos)
 		add_child(mob_book)
+		mob_book.position.y = 280 - mob_book.get_node("Sprite2D").texture.get_size().y
